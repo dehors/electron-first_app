49bf646c0748b1f8f9cb2971b1694de28692875a
slam24 <luismartinez242011@gmail.com>
2017-06-18 14:07:12 -0400

    feat: Menu and modal

--- index.js
+++ index.js
@@ -1,22 +1,99 @@
 'use strict';
 
-const electron = require('electron');  
-const app = electron.app;  
-const BrowserWindow = electron.BrowserWindow;
+const electron = require('electron');
+const { app, BrowserWindow, Menu } = electron;
+const path = require('path')
+const url = require('url')
+
+//Template for the menu
+let menuTemplate = [
+{
+	label: 'Application',
+	submenu: [
+		{
+			label: 'About',
+			click: () => {
+				openAboutWindow()
+			}
+		}
+	]
+}
+]
 
 let mainWindow;
 
-app.on('window-all-closed', function() {  
-  if(process.platform != 'darwin') {
-    app.quit();
-  }
+
+//-----------------------------Config app
+//Close all windows
+app.on('window-all-closed', function() {
+	if(process.platform != 'darwin') {
+		app.quit();
+	}
 });
 
-app.on('ready', function() {  
-  mainWindow = new BrowserWindow({width: 800, height: 600});
-  mainWindow.loadURL('file://' + __dirname + '/index.html');
+//init app
+app.on('ready', function() {
+	initWindow();
 
-  mainWindow.on('closed', function() {
-    mainWindow = null;
-  });
+	electron.powerMonitor.on('on-ac', () => {
+		mainWindow.restore()
+	})
+
+	electron.powerMonitor.on('on-battery', () => {
+		mainWindow.minimize()
+	})
 });
+
+// Reopen the app on macOS
+app.on('activate', () => {
+	if (mainWindow === null) {
+		createWindow()
+	}
+})
+
+//-----------------------------Windows
+function initWindow(argument) {
+	// Create the browser window.
+	let mainWindow = new BrowserWindow({
+		width: 1280,
+		height: 720
+	})
+
+	// Load the index.html file
+	mainWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'index.html'),
+		protocol: 'file:',
+		slashes: true
+	}))
+
+	// Set up the menu
+	var menu = Menu.buildFromTemplate(menuTemplate)
+	mainWindow.setMenu(menu)
+
+	mainWindow.on('closed', () => {
+		mainWindow = null
+	})
+}
+
+// Opens the about window
+function openAboutWindow() {
+
+	let aboutWindow = new BrowserWindow({
+		parent: mainWindow,
+		modal: true,
+		show: false,
+		width: 400,
+		height: 200
+	})
+
+	aboutWindow.loadURL(url.format({
+		pathname: path.join(__dirname, 'about.html'),
+		protocol: 'file:',
+		slashes: true
+	}))
+
+	aboutWindow.setMenu(null)
+	aboutWindow.once('ready-to-show', () => {
+		aboutWindow.show()
+	})
+}
